efc844509356f323638a8f65f36cf1dc0a9c9f94
Robert Olechowski <RobertOlechowski@gmail.com>
2019-06-01 18:49:07 +0200

    pause if error

---
+++ scripts/common_bat.bat
@@ -0,0 +1,3 @@
+set python_exe="D:\bin\Python36_64\python.exe"
+set ssh_key="D:/Archiwum/Klucze/cnc/openSSH.priv"
+set DISPLAY=127.0.0.1:0
--- start_cnc_xterm.bat
+++ start_cnc_xterm.bat
@@ -1,7 +1,11 @@
-rem @echo off
+@echo off
 cls
-set python_exe="D:\bin\Python36_64\python.exe"
-set ssh_key="D:/Archiwum/Klucze/cnc/openSSH.priv"
-set DISPLAY=127.0.0.1:0
+call scripts/common_bat
 
 %python_exe% scripts/run_x11.py -a 192.168.10.225 -k %ssh_key% -u cnc -c "xterm"
+if %ERRORLEVEL% == 0 goto :exit
+
+echo.
+pause
+
+:exit
